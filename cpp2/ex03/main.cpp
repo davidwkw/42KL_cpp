e6a901dd43f50d04d6e4f878fbde4b381e532185
David Wang <devtesting404@gmail.com>
2022-03-24 01:29:28 +0800

    Modify ex03 main.cpp

--- cpp2/ex03/main.cpp
+++ cpp2/ex03/main.cpp
@@ -6,7 +6,7 @@
 /*   By: kwang <kwang@student.42kl.edu.my>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/02/24 14:07:50 by kwang             #+#    #+#             */
-/*   Updated: 2022/03/23 12:47:18 by kwang            ###   ########.fr       */
+/*   Updated: 2022/03/24 00:47:14 by kwang            ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -16,36 +16,36 @@
 #include "Fixed.hpp"
 #include "bsp.hpp"
 
-static void check(const Point a, const Point x, const Point y, const Point z) {
-    std::cout << a << (bsp(x, y, z, a) ? " is inside" : " is outside")
-            << " Triangle XYZ" << std::endl;
-    std::cout << a << (bsp(z, y, x, a) ? " is inside" : " is outside")
-            << " Triangle ZYX" << std::endl;
+static void check(const Point point, const Point x, const Point y, const Point z) {
+    std::cout << point << (bsp(x, y, z, point) ? " is inside" : " is outside")
+            << " Triangle ABC" << std::endl;
+    std::cout << point << (bsp(z, y, x, point) ? " is inside" : " is outside")
+            << " Triangle CBA" << std::endl;
 }
 
 int main(void) {
-    Point x(0, 0);
-    Point y(0, 1);
-    Point z(1, 0);
+    Point A(-15, 0);
+    Point B(0, 15);
+    Point C(15, 0);
 
-    Point a(0, 0.5f);
-    Point b(0.5f, 1);
-    Point c(1, 0.5f);
-    Point d(0.5f, 0);
+    Point a(0, 0);
+    Point b(15, 15);
+    Point c(-15, -15);
+    Point d(0, 10.5f);
     Point e(0.5f, 0.5f);
 
-    std::cout << "x: " << x << std::endl;
-    std::cout << "y: " << y << std::endl;
-    std::cout << "z: " << z << std::endl;
+    std::cout << "A: " << A << std::endl;
+    std::cout << "B: " << B << std::endl;
+    std::cout << "C: " << C << std::endl;
 
-    check(a, x, y, z);
-    check(b, x, y, z);
-    check(c, x, y, z);
-    check(d, x, y, z);
-    check(e, x, y, z);
-    check(x, x, y, z);
-    check(y, x, y, z);
-    check(z, x, y, z);
+    check(a, A, B, C);
+    check(b, A, B, C);
+    check(c, A, B, C);
+    check(d, A, B, C);
+    check(e, A, B, C);
+    check(A, A, B, C);
+    check(B, A, B, C);
+    check(C, A, B, C);
 
     return 0;
 }
