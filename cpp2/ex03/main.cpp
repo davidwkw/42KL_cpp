96e6167cc06de94896c54795443741d7beeefb8e
David Wang <devtesting404@gmail.com>
2022-03-24 21:43:44 +0800

    Alter cpp2 ex03 tests

--- cpp2/ex03/main.cpp
+++ cpp2/ex03/main.cpp
@@ -6,7 +6,7 @@
 /*   By: kwang <kwang@student.42kl.edu.my>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/02/24 14:07:50 by kwang             #+#    #+#             */
-/*   Updated: 2022/03/24 00:47:14 by kwang            ###   ########.fr       */
+/*   Updated: 2022/03/24 21:43:23 by kwang            ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -16,11 +16,11 @@
 #include "Fixed.hpp"
 #include "bsp.hpp"
 
-static void check(const Point point, const Point x, const Point y, const Point z) {
+static void validate_point(Point const x, Point const y, Point const z, Point const point) {
     std::cout << point << (bsp(x, y, z, point) ? " is inside" : " is outside")
-            << " Triangle ABC" << std::endl;
+              << " Triangle ABC\n";
     std::cout << point << (bsp(z, y, x, point) ? " is inside" : " is outside")
-            << " Triangle CBA" << std::endl;
+              << " Triangle CBA\n";
 }
 
 int main(void) {
@@ -38,14 +38,10 @@ int main(void) {
     std::cout << "B: " << B << std::endl;
     std::cout << "C: " << C << std::endl;
 
-    check(a, A, B, C);
-    check(b, A, B, C);
-    check(c, A, B, C);
-    check(d, A, B, C);
-    check(e, A, B, C);
-    check(A, A, B, C);
-    check(B, A, B, C);
-    check(C, A, B, C);
-
+    validate_point(A, B, C, a);
+    validate_point(A, B, C, b);
+    validate_point(A, B, C, c);
+    validate_point(A, B, C, d);
+    validate_point(A, B, C, e);
     return 0;
 }
